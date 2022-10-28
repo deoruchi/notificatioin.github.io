97d7daf7940b8e4229b31d83bfc44c38e2c97839
kurokurotho <deoruchi064@gmail.com>
2022-10-28 17:59:32 +0530

    files

---
+++ script.js
@@ -0,0 +1,19 @@
+let click_blue = document.getElementsByClassName('font_bold');
+
+
+// click_blue[0].addEventListener("click",()=>{
+//     let flag=0;
+//     if(flag===0){
+//         click_blue[0].style.color="blue";
+//         flag=1;
+//     }
+//     else
+//     click_blue[0].style.color="black"
+
+// })
+
+// click_blue[0].addEventListener("click",jk());
+
+// function jk(){
+// click_blue[0].style.color="blue"
+// }
